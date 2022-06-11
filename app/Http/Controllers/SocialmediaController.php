e2314ea97598cd6b038512b84c57cdbf7e9fc22a
mbathadavid <mbathadavid3@gmail.com>
2022-06-11 14:37:02 +0300

    Check problem

--- app/Http/Controllers/SocialmediaController.php
+++ app/Http/Controllers/SocialmediaController.php
@@ -11,48 +11,48 @@ class SocialmediaController extends Controller
 {
     //Function to make sure insert a post in the database
     public function makePost(Request $req) {
-        return ['data' => $req->all()];
-    //     if($req->pcomment) {
-    //         $ppost = socialmedia::find($req->pcomment);
-    //         $value = $ppost['repliescount'];
-    //         $newval = $value + 1;
-    //         $ppost->repliescount = $newval;
-    //         $ppost->save();
-    //     }
+        // return ['data' => $req->all()];
+        if($req->pcomment) {
+            $ppost = socialmedia::find($req->pcomment);
+            $value = $ppost['repliescount'];
+            $newval = $value + 1;
+            $ppost->repliescount = $newval;
+            $ppost->save();
+        }
 
-    //     if($req->type == "reply") {
-    //         $notification = new notifications;
-    //         $user = User::find($req->uid);
-    //         $notification->uprofile = $user['profile'];
-    //         $notification->name = $user['fname'].' '.$user['lname'];
-    //         $notification->description = $user['fname'].' '.$user['lname'].' replied to your post';
-    //     }
+        if($req->type == "reply") {
+            $notification = new notifications;
+            $user = User::find($req->uid);
+            $notification->uprofile = $user['profile'];
+            $notification->name = $user['fname'].' '.$user['lname'];
+            $notification->description = $user['fname'].' '.$user['lname'].' replied to your post';
+        }
 
-    //     $post = new socialmedia;
-    //     $user = User::find($req->uid);
-    //     $post->aid = $req->uid;
-    //     $post->type = $req->type;
-    //     $post->uprofile = $user['profile'];
-    //     $post->name = $user['fname'].' '.$user['lname'];
-    //     $post->Description = $req->post;
-    //     $post->parentcomment = $req->pcomment;
+        $post = new socialmedia;
+        $user = User::find($req->uid);
+        $post->aid = $req->uid;
+        $post->type = $req->type;
+        $post->uprofile = $user['profile'];
+        $post->name = $user['fname'].' '.$user['lname'];
+        $post->Description = $req->post;
+        $post->parentcomment = $req->pcomment;
 
-    //     if ($req->hasFile('file')) {
-    //         $file = $req->file('file');
-    //         $extension = $file->getClientOriginalExtension();
-    //         $filename = time().'image'.'.'.$extension;
-    //         $file->move('images/', $filename);
+        if ($req->hasFile('file')) {
+            $file = $req->file('file');
+            $extension = $file->getClientOriginalExtension();
+            $filename = time().'image'.'.'.$extension;
+            $file->move('images/', $filename);
 
-    //         $post->images = $filename;
-    //         } 
+            $post->images = $filename;
+            } 
         
 
-    //    $post->save();
+       $post->save();
 
-    //     return response()->json([
-    //         'status' => 200,
-    //         'message' => 'Posted Successfully'
-    //     ]);
+        return response()->json([
+            'status' => 200,
+            'message' => 'Posted Successfully'
+        ]);
     }
 
     //Funtion to fetch posts
