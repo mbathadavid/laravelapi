09c1002c3f30147bb9b90b67c2ddb90d35bbcdf1
mbathadavid <mbathadavid3@gmail.com>
2022-06-13 00:17:03 +0300

    Work on updating

--- app/Http/Controllers/SocialmediaController.php
+++ app/Http/Controllers/SocialmediaController.php
@@ -27,6 +27,7 @@ class SocialmediaController extends Controller
             $notification->uprofile = $user['profile'];
             $notification->name = $user['fname'].' '.$user['lname'];
             $notification->description = $user['fname'].' '.$user['lname'].' replied to your post';
+            $notification->save();
         }
 
         $post = new socialmedia;
